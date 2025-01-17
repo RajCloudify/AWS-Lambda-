31cb148a60a7382d736d203d62c87f4f998ef29d
RajCloudify <rajattingal1@gmail.com>
2025-01-17 19:46:32 +0400

    Create s3.py

---
+++ s3.py
@@ -0,0 +1,11 @@
+#create s3
+
+import boto3
+client=boto3.client('s3')
+response = client.create_bucket(
+    Bucket='qwertyuioasdfgh',
+
+    CreateBucketConfiguration={
+        'LocationConstraint': "ap-south-1",
+}
+)
