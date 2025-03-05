bc4d755881013f24fbb13a141a003f3f963693b5
Raj Attingal <rajattingal1@gmail.com>
2025-03-05 10:35:54 +0400

    Creating Lambda Function and calling zip file from S3

--- lambda-function.py
+++ lambda-function.py
@@ -1,12 +1,13 @@
 #To create a AWS Lambda Function 
+#Note:- It will create a lambda function and and add the function from the zip file in S3 bucket it won't upload the zip file in S3. The zip file should already be present in S3
 
 import boto3
 
-lambda_client = boto3.client('lambda', region_name='us-east-1')
+lambda_client = boto3.client('lambda', region_name='ap-south-1')
 
 response = lambda_client.create_function(
     Code={
-        'S3Bucket': 'nareshawsdev',
+        'S3Bucket': 'rajsbbuckett',
         'S3Key': 'lambda.zip',  # Use the name of your ZIP file
     },
     Description='Process image objects from Amazon S3.',
