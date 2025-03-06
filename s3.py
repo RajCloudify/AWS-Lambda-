dbb4b6dbffbce914958e08e6cef8be01255e2315
Raj Attingal <rajattingal1@gmail.com>
2025-03-06 17:37:56 +0400

    S3 Creation using boto3

--- s3.py
+++ s3.py
@@ -1,9 +1,9 @@
-#create s3
+#create s3 bucket using boto3
 
 import boto3
 client=boto3.client('s3')
 response = client.create_bucket(
-    Bucket='qwertyuioasdfgh',
+    Bucket='qwertyuioasdfgh', #bucket name
 
     CreateBucketConfiguration={
         'LocationConstraint': "ap-south-1",
