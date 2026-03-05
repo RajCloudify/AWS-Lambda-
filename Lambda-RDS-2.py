f67f7d4d8a5f226bdc153b118a55a6b6060322b7
Raj Attingal <rajattingal1@gmail.com>
2026-03-05 06:52:44 +0400

    Fix table name in Lambda-RDS-2.py

--- Lambda-RDS-2.py
+++ Lambda-RDS-2.py
@@ -3,7 +3,7 @@ import pymysql
 import json
 
 # Database settings from environment variables 
-db_host = os.environ['DB_HOST']
+db_host = os.environ['DB_HOST']  
 db_user = os.environ['DB_USER']
 db_pass = os.environ['DB_PASS']
 new_db_name = "EmployeeDB"  # Database Name
